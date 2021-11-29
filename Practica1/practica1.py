17d86d2f05dd6abdabae4538007d82aad91b5b56
ayshagandarova <50736989+ayshagandarova@users.noreply.github.com>
2021-11-29 14:23:44 +0100

    Perfecto (comprobar prints)

--- Practica1/practica1.py
+++ Practica1/practica1.py
@@ -21,11 +21,12 @@ PERSONES = HOMES + DONES
 VEGADES_AL_BANY = 2
 
 # SEMÁFOROS
-semContadorDones = threading.Semaphore(3) 
+semContadorDones = threading.Semaphore(3)  # explicar que hace cada semaforo
 semContadorHomes = threading.Semaphore(3)  
 semDones = threading.Semaphore(1) 
 semHomes = threading.Semaphore(1) 
 semBuit = threading.Semaphore(1)
+semInan = threading.Semaphore(1)
 
 # VARIABLES AUXILIARES
 contadorHomes = 0
@@ -38,57 +39,59 @@ nomsDones = ["AINA", "GERONIA", "CATALINA", "ELISABET", "JOANA", "FRANCESCA"]
 # MÉTODO "RUN" DE MUJERES
 def dona():
     global contadorDones
-    semDones.acquire()
-    threading.current_thread().name = nomsDones.pop()
-    semDones.release()
 
     # Se prepara para entrar a trabajar al despacho
     print("\t"+ threading.current_thread().name + " arriba al despatx")
-    time.sleep(random.randint(5, 10) / 10000) 
+
 
     # Los dos accesos de los procesos "hilo" de las mujeres al baño
     for i in range(VEGADES_AL_BANY): 
 
         # Se pone a trabajar
         print("\t"+ threading.current_thread().name + " treballa")
-        time.sleep(random.randint(5, 10) / 10000) # esta trabajando
+        time.sleep(random.randint(5, 10) / 10000)
+        
+        semInan.acquire()
 
         # Comprueba que el baño está vacío y desocupado por el otro género
         semDones.acquire() 
         if contadorDones == 0:
             semBuit.acquire()
+        contadorDones += 1
         semDones.release()
-        
+
+        semInan.release()
+
+        #SECCIÓN CRÍTICA
         # Permite la entrada de un máximo de tres mujeres
         semContadorDones.acquire()
-
         # Incrementamos la correspondiente variable de control de mujeres dentro del baño,
         # además de imprimir el mensaje correspondiente
         semDones.acquire()
-        contadorDones += 1
         print("\t" + threading.current_thread().name + " entra " + str(i+1) + "/2. Personas en el baño: ", contadorDones) 
         semDones.release()
 
         time.sleep(random.randint(5, 10) / 10000) # Se encuentra en el baño
+
+        # Desbloqueamos la entrada de control de un máximo de tres mujeres
+        semContadorDones.release()
+        #FIN SECCIÓN CRÍTICA
 
         # Decrementamos la correspondiente variable de control de mujeres dentro del baño,
         # además de imprimir el mensaje correspondiente
         semDones.acquire()
         contadorDones -= 1
         print("\t" + threading.current_thread().name + " surt.")
-        semDones.release()
-
-        # Desbloqueamos la entrada de control de un máximo de tres mujeres
-        semContadorDones.release()
+        
 
         # Comprueba que el baño está vacío y lo desbloquea para el otro género
-        semDones.acquire()
+     
         if contadorDones == 0:
             print("***** El baño esta vacio")
             semBuit.release()
         semDones.release()
+        time.sleep(random.randint(5, 10) / 10000)
 
-        time.sleep(random.randint(5, 10) / 1000) # Vuelve al trabajo
 
     # Acaba de trabajar después de haber entrado dos veces al baño
     print("\t" + threading.current_thread().name + " acaba la feina")
@@ -96,26 +99,26 @@ def dona():
 # MÉTODO "RUN" DE HOMBRES
 def home():
     global contadorHomes
-    semHomes.acquire()
-    threading.current_thread().name = nomsHomes.pop()
-    semHomes.release()
     
     # Se prepara para entrar a trabajar al despacho
     print(threading.current_thread().name + " arriba al despatx")
-    time.sleep(random.randint(5, 10) / 10000)    
 
     # Los dos accesos de los procesos "hilo" de los hombres al baño
     for i in range(VEGADES_AL_BANY): 
 
         # Se pone a trabajar
         print(threading.current_thread().name + " treballa")
-        time.sleep(random.randint(5, 10) / 10000) 
 
+
+        semInan.acquire()
         # Comprueba que el baño está vacío y desocupado por el otro género
         semHomes.acquire()
         if contadorHomes == 0:
             semBuit.acquire()
+        contadorHomes += 1
         semHomes.release()
+
+        semInan.release()
 
         # Permite la entrada de un máximo de tres hombres
         semContadorHomes.acquire()
@@ -123,30 +126,28 @@ def home():
         # Incrementamos la correspondiente variable de control de hombres dentro del baño,
         # además de imprimir el mensaje correspondiente
         semHomes.acquire()
-        contadorHomes += 1
+        
         print(threading.current_thread().name + " entra " + str(i+1) + "/2. Personas en el baño: ", contadorHomes) 
         semHomes.release()
 
         time.sleep(random.randint(5, 10) / 10000) # Se encuentra en el baño
+
+         # Desbloqueamos la entrada de control de un máximo de tres hombres
+        semContadorHomes.release()
 
         # Decrementamos la correspondiente variable de control de hombres dentro del baño,
         # además de imprimir el mensaje correspondiente
         semHomes.acquire()
         contadorHomes -= 1
         print(threading.current_thread().name + " surt.")
-        semHomes.release()
-
-        # Desbloqueamos la entrada de control de un máximo de tres hombres
-        semContadorHomes.release()
 
         # Comprueba que el baño está vacío y lo desbloquea para el otro género
-        semHomes.acquire()
+
         if contadorHomes == 0:
             print("***** El baño esta vacio")
             semBuit.release()
         semHomes.release()
 
-        time.sleep(random.randint(5, 10) / 1000) # Vuelve al trabajo
 
     # Acaba de trabajar después de haber entrado dos veces al baño
     print(threading.current_thread().name + " acaba la feina")
@@ -158,13 +159,13 @@ def main():
 
     # Lanzamos los procesos "hilo" que serán los hombres
     for i in range(HOMES):
-        t = threading.Thread(target=home)
+        t = threading.Thread(target=home, name=nomsHomes[i])
         threads.append(t)
         t.start()
 
     # Lanzamos los procesos "hilo" que serán las mujeres
     for i in range(DONES):
-        t = threading.Thread(target=dona)
+        t = threading.Thread(target=dona, name=nomsDones[i])
         threads.append(t)
         t.start()
 
@@ -172,7 +173,8 @@ def main():
     for t in threads:
         t.join()
 
-    print("End")
+    print("-----------------------------------------End-----------------------------------------")
+
 
 # DECLARACIÓN PROGRAMA PRINCIPAL
 if __name__ == "__main__":
